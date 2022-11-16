8ec8a99d05129a2d515b57510e7494f90e8dabf0
Owen Shepherd <owen@owen.cafe>
2022-11-16 16:51:36 +0000

    NoInline support

--- src/Pinch/Generate.hs
+++ src/Pinch/Generate.hs
@@ -264,7 +264,7 @@ gEnum e = do
       , H.FunBind (toEnum' ++ [toEnumDef])
       ]
     , H.InstDecl (H.InstHead [] clHashable (H.TyCon tyName))
-        [ H.FunBind
+        [ H.PragmaFunBind [H.PNoInline]
           [ H.Match "hashWithSalt" [H.PVar "s", H.PVar "x"] 
             $ H.EApp (H.EVar "hashWithSalt")
               [ H.EVar "s"
