ecafde990dde0fdc23879fca6508ca425b685fad
Owen Shepherd <owen@owen.cafe>
2024-07-01 11:55:34 +0100

    wip

--- src/Pinch/Generate.hs
+++ src/Pinch/Generate.hs
@@ -446,13 +446,13 @@ gService s = do
   (nms, tys, handlers, calls, tyDecls) <- unzip5 <$> traverse gFunction (serviceFunctions s)
   let serverDecls =
         [ H.DataDecl "APIVersion" [] ["Basic", "WithHeaders"] []
-        , H.ClosedTypeFamily "APIReturn" ["(a :: APIVersion)"]
-          [ (H.PCon "WithHeaders" [], H.ETuple ["a", "Pinch.Internal.HeaderData"])
-          , (H.PCon "Basic" [], "a")
+        , H.ClosedTypeFamily "APIReturn" ["(a :: APIVersion)", "r"]
+          [ (H.PCon "'WithHeaders" ["r"], H.ETuple ["r", "Pinch.Internal.HeaderData"])
+          , (H.PCon "'Basic" ["r"], "r")
           ]
         , H.DataDecl (serviceTyName <> "Generic") ["(apiVersion :: APIVersion)"] [ H.RecConDecl serviceConName $ zip nms tys ] []
-        , H.TypeDecl (H.TyCon $ serviceTyName <> "'") $ H.TyApp (H.TyCon $ serviceTyName <> "Generic") ["WithHeaders"]
-        , H.TypeDecl (H.TyCon $ serviceTyName <> "") $ H.TyApp (H.TyCon $ serviceTyName <> "Generic") ["Basic"]
+        , H.TypeDecl (H.TyCon $ serviceTyName <> "'") $ H.TyApp (H.TyCon $ serviceTyName <> "Generic") ["'WithHeaders"]
+        , H.TypeDecl (H.TyCon $ serviceTyName <> "") $ H.TyApp (H.TyCon $ serviceTyName <> "Generic") ["'Basic"]
         , H.TypeSigDecl (prefix <> "_mkServer") (H.TyLam [H.TyCon serviceConName] (H.TyCon "Pinch.Server.ThriftServer"))
         , H.FunBind
           [ H.Match (prefix <> "_mkServer") [H.PVar "server"]
