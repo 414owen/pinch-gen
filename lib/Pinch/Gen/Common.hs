23d7dfff3896ac72ba4c4b98094499b3efb672bf
Owen Shepherd <owen@owen.cafe>
2024-07-11 15:10:57 +0200

    common wip

--- lib/Pinch/Gen/Common.hs
+++ lib/Pinch/Gen/Common.hs
@@ -7,6 +7,8 @@
 {-# LANGUAGE FlexibleInstances     #-}
 {-# LANGUAGE AllowAmbiguousTypes   #-}
 {-# LANGUAGE FlexibleContexts      #-}
+{-# LANGUAGE DerivingStrategies    #-}
+{-# LANGUAGE TypeOperators #-}
 
 module Pinch.Gen.Common
   ( APIVersion(..)
@@ -17,6 +19,8 @@ module Pinch.Gen.Common
 
 import qualified Pinch.Transport as Transport
 import Pinch.Internal.RPC (ThriftResult(..))
+import Pinch (Pinchable(..))
+import Pinch.Internal.TType (TStruct)
 
 data APIVersion 
   = Basic
@@ -26,15 +30,31 @@ type family APIReturn (a :: APIVersion) r where
   APIReturn 'WithHeaders r = (r, Transport.HeaderData)
   APIReturn 'Basic r = r
 
-class LiftReturn (apiVersion :: APIVersion) r r' where
+class ThriftResult r' => LiftReturn (apiVersion :: APIVersion) r r' where
   liftReturn :: r -> (r', Transport.HeaderData)
 
-instance LiftReturn 'WithHeaders r r where
+instance ThriftResult r => LiftReturn 'WithHeaders r r where
   liftReturn  = (, Transport.emptyHeaderData)
 
-instance LiftReturn 'Basic (r, Transport.HeaderData) r where
+instance ThriftResult r => LiftReturn 'Basic (r, Transport.HeaderData) r where
   liftReturn  = Prelude.id
 
-liftWrap :: forall (apiVersion :: APIVersion) a. (LiftReturn apiVersion a a, ThriftResult a) => IO (ResultType a) -> IO (a, Transport.HeaderData)
-liftWrap act = liftReturn @apiVersion <$> wrap @a act
+newtype PinchHeaderWrapper a = PinchHeaderWrapper (a, Transport.HeaderData)
 
+instance Pinchable a => Pinchable (PinchHeaderWrapper a) where
+  type Tag (PinchHeaderWrapper a) = Tag a
+  pinch (PinchHeaderWrapper (a, _)) = pinch a
+  unpinch a = PinchHeaderWrapper . (, Transport.emptyHeaderData) <$> unpinch a
+  
+instance ThriftResult a => ThriftResult (PinchHeaderWrapper a) where
+  type ResultType (PinchHeaderWrapper a) = ResultType a
+  wrap m = PinchHeaderWrapper . (, Transport.emptyHeaderData) <$> wrap m
+  unwrap (PinchHeaderWrapper (a, _)) = unwrap a
+
+-- wrap :: IO (ResultType a) -> IO a
+-- liftReturn :: r -> (r', Transport.HeaderData)
+
+liftWrap :: forall (apiVersion :: APIVersion) a.
+  (ThriftResult (APIReturn apiVersion a), LiftReturn apiVersion (APIReturn apiVersion a) a)
+  => IO (ResultType (APIReturn apiVersion a)) -> IO (a, Transport.HeaderData)
+liftWrap act = liftReturn @apiVersion <$> wrap @(APIReturn apiVersion a) act
